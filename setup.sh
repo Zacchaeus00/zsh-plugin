63c49f5fe96578a4d97b75eca3a831ca4ca2bc01
Zacchaeus14 <yw3642@nyu.edu>
2022-06-02 05:53:25 +0800

    update

--- setup.sh
+++ setup.sh
@@ -1,6 +1,6 @@
 sed -i "" "s/plugins=(git)/plugins=(git sublime autojump zsh-autosuggestions zsh-syntax-highlighting python)/" ~/.zshrc
-git clone git://github.com/wting/autojump.git
-cd autojump
+git clone git://github.com/wting/autojump.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/autojump
+cd ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/autojump
 ./install.py
 git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
