436e46bbeb1f3460b1231c048792d92b06fd8179
Yuchen Wang <64816317+Zacchaeus14@users.noreply.github.com>
2022-06-07 00:31:42 +0800

    Create setup_linux.sh

---
+++ setup_linux.sh
@@ -0,0 +1,7 @@
+
+sed -i "s/plugins=(git)/plugins=(git sublime autojump zsh-autosuggestions zsh-syntax-highlighting python web-search)/" ~/.zshrc
+git clone https://github.com/wting/autojump.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/autojump
+cd ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/autojump
+./install.py
+git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
